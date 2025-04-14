c61c3880b33d9234df5dd4203cf8f6f69a6f41ce
limagln <gabriellneiva@outlook.com>
2025-04-14 20:51:55 -0300

    Remove virtual override

--- AgendaCorp/Models/Evento.cs
+++ AgendaCorp/Models/Evento.cs
@@ -18,9 +18,9 @@ namespace AgendaCorp.Models
 		public DateTime Data { get; set; }
 		public string Modalidade { get; set; }
 
-		public virtual ICollection<Palestrante>? Palestrantes { get; set; } = new List<Palestrante>();
+		public ICollection<Palestrante>? Palestrantes { get; set; } = new List<Palestrante>();
 		[DisplayName("Inscrições")]
-		public virtual ICollection<Inscricao>? Inscricoes { get; set; } = new List<Inscricao>();
+		public ICollection<Inscricao>? Inscricoes { get; set; } = new List<Inscricao>();
 
 	}
 }
