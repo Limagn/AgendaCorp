95f743be522810cb650584909031f6612c238636
limagln <gabriellneiva@outlook.com>
2025-05-26 20:17:35 -0300

    Add partial view Inscricoes

--- AgendaCorp/Models/Inscricao.cs
+++ AgendaCorp/Models/Inscricao.cs
@@ -5,12 +5,15 @@ namespace AgendaCorp.Models
 	public class Inscricao
 	{
 		public int InscricaoId { get; set; }
+
 		[DisplayName("Evento")]
 		public int EventoId { get; set; }
 		public Evento? Evento { get; set; }
+
 		[DisplayName("Participante")]
 		public int ParticipanteId { get; set; }
 		public Participante? Participante { get; set; }
+
 		[DisplayName("Data de Inscrição")]
 		public DateTime DataInscricao { get; set; } = DateTime.Now;
 		public string Status { get; set; }
