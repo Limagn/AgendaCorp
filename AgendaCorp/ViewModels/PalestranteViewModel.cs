5babd8b0118a2ced0217616ea97c8a09c166d25e
limagln <gabriellneiva@outlook.com>
2025-05-31 23:18:33 -0300

    Update Palestrante views Create and Edit

---
+++ AgendaCorp/ViewModels/PalestranteViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgendaCorp.ViewModels
+{
+	public class PalestranteViewModel
+	{
+		public int? PalestranteId { get; set; }
+
+		[Required]
+		[DisplayName("Nome Completo")]
+		public string Nome { get; set; }
+
+		[DisplayName("E-mail")]
+		public string Email { get; set; }
+
+		public string Telefone { get; set; }
+
+		[DisplayName("Área de Atuação")]
+		public string Area { get; set; }
+
+		[DisplayName("Eventos")]
+		public List<int> EventoIds { get; set; } = new();
+
+		public List<SelectListItem>? EventosSelectList { get; set; }
+
+
+	}
+}
