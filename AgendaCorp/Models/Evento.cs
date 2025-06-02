4d9c145f969213d9df6d76d8d1d9f89623d1afd9
limagln <gabriellneiva@outlook.com>
2025-06-02 09:31:34 -0300

    Update Eventos nullable fields

--- AgendaCorp/Models/Evento.cs
+++ AgendaCorp/Models/Evento.cs
@@ -10,12 +10,12 @@ namespace AgendaCorp.Models
 		[DisplayName("Nome do Evento")]
 		public string Nome { get; set; }
 		public string Local { get; set; }
-		public string Cep { get; set; }
-		public string Logradouro { get; set; }
-		public string Bairro { get; set; }
-		public string Cidade { get; set; }
+		public string? Cep { get; set; }
+		public string? Logradouro { get; set; }
+		public string? Bairro { get; set; }
+		public string? Cidade { get; set; }
 		[DisplayName("Estado")]
-		public string Uf { get; set; }
+		public string? Uf { get; set; }
 		[Required]
 		[DisplayName("Data do Evento")]
 		public DateTime Data { get; set; }
